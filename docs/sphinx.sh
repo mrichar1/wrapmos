f0567ae82d2190a6ecebdfc6d4c69bd365e7a87f
Matthew Richardson <M.Richardson@ed.ac.uk>
2019-05-20 16:39:25 +0100

    Always build docs for master.

--- docs/sphinx.sh
+++ docs/sphinx.sh
@@ -10,7 +10,7 @@ TARGET=docs/build
 sphinx-apidoc -f -T -e -o ${SOURCE}/apidoc wrapmos
 travis-sphinx --outdir=${TARGET} build --source=${SOURCE}
 # Build docs if this is master branch, and HEAD has a tag associated with it
-if [[ $TRAVIS_BRANCH == "master" ]] && git describe --exact-match HEAD; then
+if [[ $TRAVIS_BRANCH == "master" ]]; then
   echo "Deploying docs to gh-pages..."
   travis-sphinx --outdir=${TARGET} deploy
 fi
