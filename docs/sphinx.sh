5e4a0f1f6bc42b29bd56cbb67d8e2b50dbc56a67
Matthew Richardson <M.Richardson@ed.ac.uk>
2019-05-20 16:20:36 +0100

    Add tavis doc-building.

--- docs/sphinx.sh
+++ docs/sphinx.sh
@@ -1,3 +1,16 @@
-sphinx-apidoc -f -o source/apidoc ..
-make clean
-make html
+#!/bin/bash
+
+# Try to ensure we're in the project 'root' for relative paths etc to work.
+cd "$(dirname $0)/.."
+
+PATH=bin/:$PATH
+SOURCE=docs/source
+TARGET=docs/build
+
+sphinx-apidoc -f -T -e -o ${SOURCE}/apidoc wrapmos
+travis-sphinx --outdir=${TARGET} build --source=${SOURCE}
+# Build docs if this is master branch, and HEAD has a tag associated with it
+if [[ $TRAVIS_BRANCH == "master" ]] && git describe --exact-match HEAD; then
+  echo "Deploying docs to gh-pages..."
+  travis-sphinx --outdir=${TARGET} deploy
+fi
